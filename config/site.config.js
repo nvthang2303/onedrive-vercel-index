f2a02df4c126a53a0e688fc5be03da2274fb480f
Thang Nguyen <97169418+nvthang2303@users.noreply.github.com>
2023-07-02 12:44:42 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,11 +52,10 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      name: 'Telegram',
+      link: 'https://t.me/MiuiTNChannel',
     },
   ],
-
   // This is a day.js-style datetime format string to format datetimes in the app. Ref to
   // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
   // without timezone and replacing T with space.
